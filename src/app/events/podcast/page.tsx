0f7ca8956d92a94c482259d6da65c323041008b1
Vishal Panchal <vishal.panchal@bypt.in>
2025-04-17 15:43:48 +0530

    Added the toolkit and pathfinder popup form

--- src/app/events/podcast/page.tsx
+++ src/app/events/podcast/page.tsx
@@ -1,9 +1,59 @@
+'use client';
 import Image from "next/image";
 import curve1 from "../../../../src/app/about/images/curve1.png";
 import bluecurve from "../basecamp/Images/blue-curve.svg";
 import basecamplogo2 from "../podcast/images/Pathfinders_White.png";
+import Toast from "../../../components/Toast";
+import { useRouter } from 'next/navigation';
+import Link from "next/link";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Spinner from 'react-bootstrap/Spinner';
 import "./podcast.css";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 const page = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const router = useRouter();
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const onSubmit = async (data: any) => {
+    setShowSpinner(true);
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL + "/path-finder"; // Replace with your API URL
+    const postData: any = data;
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+      //console.log("response", response);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      reset();
+      handleClose();
+      Toast.success("Your request for pathfinders or masterminds speaker has been submitted!");
+      setShowSpinner(false);
+    
+      //setResponseData(data);
+    } catch (error) {
+      console.error("Error:", error);
+      Toast.error("An error occurred while submitting the form.");
+    }
+  };
   return (
     <div className="main_body_div">
       {/* <div className="first_div_layout podcast">
@@ -25,15 +75,18 @@ const page = () => {
       <div className="basecamp-hero-section">
         <Image className="blue-curve" src={bluecurve} alt="img2" />
         <div className="hero-content">
-        <div className="hero-icon-img">
+          <div className="hero-icon-img">
             <Image className="logo-image" src={basecamplogo2} alt="img2" />
           </div>
           <p className="heading" style={{ color: "white" }}>
-          Pathfinders Podcast
+            Pathfinders Podcast
           </p>
           <p className="subText" style={{ color: "white" }}>
-          A podcast series where we delve into the inspiring journeys of successful business leaders, founders, and CEOs of Small and Medium-sized enterprises. 
+            A podcast series where we delve into the inspiring journeys of successful business leaders, founders, and CEOs of Small and Medium-sized enterprises.
           </p>
+          <p><Button variant="primary" onClick={handleShow}>
+        To become Pathfinders
+      </Button></p>
         </div>
       </div>
       <div className="seventh_components shadow-lg">
@@ -56,7 +109,7 @@ const page = () => {
                 </div>
                 <div className="card-body">
                   <h5 className="card-title">
-                  Success Alchemists Pathfinders
+                    Success Alchemists Pathfinders
                   </h5>
                 </div>
               </div>
@@ -80,7 +133,7 @@ const page = () => {
                 </div>
                 <div className="card-body">
                   <h5 className="card-title">
-                   Mentoring Startups towards success
+                    Mentoring Startups towards success
                   </h5>
                 </div>
               </div>
@@ -104,7 +157,7 @@ const page = () => {
                 </div>
                 <div className="card-body">
                   <h5 className="card-title">
-                  Entrepreneurial insights from fintech leader
+                    Entrepreneurial insights from fintech leader
                   </h5>
                 </div>
               </div>
@@ -394,6 +447,186 @@ const page = () => {
           </div>
         </div>
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Become Pathfinders or masterminds speaker</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.name ? "is-invalid" : ""
+                  }`}
+                id="name"
+                placeholder="Name"
+                {...register("name", {
+                  required: "Name is required",
+
+                })}
+              />
+              <label htmlFor="name">Name</label>
+              {errors.name && (
+                <div className="invalid-feedback">
+                  {String(errors.name.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.email ? "is-invalid" : ""
+                  }`}
+                id="email"
+                placeholder="Email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Invalid email address",
+                  },
+
+                })}
+              />
+              <label htmlFor="email">Email</label>
+              {errors.email && (
+                <div className="invalid-feedback">
+                  {String(errors.email.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.phone ? "is-invalid" : ""
+                  }`}
+                id="phone"
+                placeholder="Phone"
+                {...register("phone", {
+                  required: "Phone is required",
+                })}
+              />
+              <label htmlFor="phone">Phone</label>
+              {errors.phone && (
+                <div className="invalid-feedback">
+                  {String(errors.phone.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.company ? "is-invalid" : ""
+                  }`}
+                id="company"
+                placeholder="Company Name"
+                {...register("company", {
+                  required: "Company Name is required",
+                })}
+              />
+              <label htmlFor="company">Company Name</label>
+              {errors.company && (
+                <div className="invalid-feedback">
+                  {String(errors.company.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.jobTitle ? "is-invalid" : ""
+                  }`}
+                id="jobTitle"
+                placeholder="Job Title"
+                {...register("jobTitle", {
+                  required: "Job Title is required",
+                })}
+              />
+              <label htmlFor="jobTitle">Job Title</label>
+              {errors.jobTitle && (
+                <div className="invalid-feedback">
+                  {String(errors.jobTitle.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.linkedinProfile ? "is-invalid" : ""
+                  }`}
+                id="linkedinProfile"
+                placeholder="LinkedIn Profile"
+                {...register("linkedinProfile", {
+                  required: "LinkedIn Profile is required",
+                })}
+              />
+              <label htmlFor="linkedinProfile">LinkedIn Profile</label>
+              {errors.linkedinProfile && (
+                <div className="invalid-feedback">
+                  {String(errors.linkedinProfile.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <select {...register("interested")} className={`form-select ${errors.interested ? "is-invalid" : ""}`} id="interested" aria-label="Default select example">
+                <option selected value="Pathfinders">Pathfinders</option>
+                <option value="Masterminds">Masterminds</option>  
+                <option value="Any">Any</option>  
+              </select>
+              <label htmlFor="interested">Are you interested in speaking on ?</label>
+              {errors.interested && (
+                <div className="invalid-feedback">
+                  {String(errors.interested.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <select {...register("expertise")} className={`form-select ${errors.expertise ? "is-invalid" : ""}`} id="interested" aria-label="Default select example">
+                <option selected value="Scaling Business">Scaling Business</option>
+                <option value="Leadership">Leadership</option>  
+                <option value="Startup Growth">Startup Growth</option>  
+                <option value="Operations">Operations</option> 
+                <option value="Fundraising">Fundraising</option>  
+              </select>
+              <label htmlFor="expertise">Preferred topics or Area of expertise</label>
+              {errors.expertise && (
+                <div className="invalid-feedback">
+                  {String(errors.expertise.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.reason ? "is-invalid" : ""
+                  }`}
+                id="reason"
+                placeholder="Why do you want to be a speaker on our platform?"
+                {...register("reason", {
+                  required: "Reason is required",
+                })}
+              />
+              <label htmlFor="reason">Why do you want to be a speaker on our platform?</label>
+              {errors.reason && (
+                <div className="invalid-feedback">
+                  {String(errors.reason.message)}
+                </div>
+              )}
+            </div>
+            
+            
+
+
+          </Modal.Body>
+          <Modal.Footer>
+            {showSpinner && <Spinner animation="border" variant="warning" />}
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </Modal.Footer>
+        </form>
+      </Modal>
     </div>
   );
 };
--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import "./resources.css";
 import Case1 from "./images/tools-final-08.png";
 import Case2 from "./images/tools-final-09.png";
@@ -10,11 +10,13 @@ import Book2 from "../../app/resources/images/ref-book2.png";
 import Book3 from "../../app/resources/images/ref-book3.png";
 import Book4 from "../../app/resources/images/ref-book4.png";
 import Image from "next/image";
-import curve1 from "../../app/resources/images/curve1.png";
 import { useForm } from "react-hook-form";
 import Toast from "../../components/Toast";
 import { useRouter } from 'next/navigation';
 import Link from "next/link";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Spinner from 'react-bootstrap/Spinner';
 
 const page = () => {
   const {
@@ -24,37 +26,51 @@ const page = () => {
     formState: { errors },
   } = useForm();
   const router = useRouter();
-  const onSubmit = async(data: any) => {
-    //console.log("Submitted Data:", data.email);
-    //console.log("APP", process.env.NEXT_PUBLIC_API_URL);
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL+"/newsletter"; // Replace with your API URL
-        const postData:any = data;
-    
-        try {
-          const response = await fetch(apiUrl, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(postData),
-          });
-          //console.log("response", response);
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-    
-          const data = await response.json();
-          reset();
-          Toast.success("Your request for newseletter has been submitted!");
-          setTimeout(() => {
-            router.push("/"); // Redirect to the home page after 2 seconds  
-          }, 2000);
-          
-          //setResponseData(data);
-        } catch (error) {
-          console.error("Error:", error);
-          Toast.error("An error occurred while submitting the form.");
-        }
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleFormSubmit = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault(); // Prevent the default anchor click behavior
+    //handleShow(); // Show the modal when the link is clicked
+    localStorage.getItem('hasSubmittedInquiry') === 'true' ? router.push("/toolDetail") : handleShow();
+  };
+
+  const onSubmit = async (data: any) => {
+    setShowSpinner(true);
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL + "/toolkit"; // Replace with your API URL
+    const postData: any = data;
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+      //console.log("response", response);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      reset();
+      handleClose();
+      localStorage.setItem('hasSubmittedInquiry', 'true');
+      Toast.success("Your request for toolkit download has been registered! You can download now.");
+      setShowSpinner(false);
+      setTimeout(() => {
+        router.push("/toolDetail"); // Redirect to the home page after 2 seconds  
+      }, 2000);
+
+      //setResponseData(data);
+    } catch (error) {
+      console.error("Error:", error);
+      Toast.error("An error occurred while submitting the form.");
+    }
   };
   return (
     <div className="main_body_div">
@@ -79,7 +95,7 @@ const page = () => {
           <p className="section-header">Our latest tool kit</p>
           {/*<button
             className="border-0 bg-transparent text-decoration-underline"
-            style={{ color: "rgba(251, 168, 25, 1)" }}
+            style={{ color: "rgba(251, 168, 25, 1)" }} handleClose
           >
             VIEW MORE
   </button>*/}
@@ -87,7 +103,7 @@ const page = () => {
         <div className="mt-4">
           <div className="row g-4">
             <div className="col-xxl-3 col-lg-3 col-md-6 col-sm-12 image-div">
-              <Link className="tool-img-link" href="/toolDetail">
+              <Link className="tool-img-link" onClick={handleFormSubmit} href="javascript:void(0);" >
                 <Image
                   src={Case2}
                   alt="case1"
@@ -101,7 +117,7 @@ const page = () => {
               </Link>
             </div>
             <div className="col-xxl-3 col-lg-3 col-md-6 col-sm-12 image-div">
-              <Link className="tool-img-link" href="/toolDetail">
+              <Link className="tool-img-link" onClick={handleFormSubmit} href="javascript:void(0);" >
                 <Image
                   src={Case1}
                   alt="case2"
@@ -114,7 +130,7 @@ const page = () => {
               </Link>
             </div>
             <div className="col-xxl-3 col-lg-3 col-md-6 col-sm-12 image-div">
-              <Link className="tool-img-link" href="/toolDetail">
+              <Link className="tool-img-link" onClick={handleFormSubmit} href="javascript:void(0);" >
                 <Image
                   src={Case3}
                   alt="case3"
@@ -127,7 +143,7 @@ const page = () => {
               </Link>
             </div>
             <div className="col-xxl-3 col-lg-3 col-md-6 col-sm-12 image-div">
-              <Link className="tool-img-link" href="/toolDetail">
+              <Link className="tool-img-link" onClick={handleFormSubmit} href="javascript:void(0);" >
                 <Image
                   src={Case4}
                   alt="case3"
@@ -268,6 +284,102 @@ const page = () => {
           </div>
         </form>
       </div> */}
+
+      <Modal show={show} onHide={handleClose}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Toolkit Assessement</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.name ? "is-invalid" : ""
+                  }`}
+                id="name"
+                placeholder="Name"
+                {...register("name", {
+                  required: "Name is required",
+
+                })}
+              />
+              <label htmlFor="name">Name</label>
+              {errors.name && (
+                <div className="invalid-feedback">
+                  {String(errors.name.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.name ? "is-invalid" : ""
+                  }`}
+                id="email"
+                placeholder="Email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Invalid email address",
+                  },
+
+                })}
+              />
+              <label htmlFor="email">Email</label>
+              {errors.email && (
+                <div className="invalid-feedback">
+                  {String(errors.email.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.name ? "is-invalid" : ""
+                  }`}
+                id="phone"
+                placeholder="Phone"
+                {...register("phone", {
+                  required: "Phone is required",
+                })}
+              />
+              <label htmlFor="phone">Phone</label>
+              {errors.phone && (
+                <div className="invalid-feedback">
+                  {String(errors.phone.message)}
+                </div>
+              )}
+            </div>
+            <div className="form-floating m-3">
+              <input
+                type="text"
+                className={`form-control ${errors.name ? "is-invalid" : ""
+                  }`}
+                id="company"
+                placeholder="Company Name"
+                {...register("company", {
+                  required: "Company Name is required",
+                })}
+              />
+              <label htmlFor="company">Company Name</label>
+              {errors.company && (
+                <div className="invalid-feedback">
+                  {String(errors.company.message)}
+                </div>
+              )}
+            </div>
+
+          </Modal.Body>
+          <Modal.Footer>
+            { showSpinner && <Spinner animation="border" variant="warning" /> }
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </Modal.Footer>
+        </form>
+      </Modal>
     </div>
   );
 };
