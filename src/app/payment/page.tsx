3f278021dce7d287010c45a7070ec30503cac604
Vishal Panchal <vishal.panchal@bypt.in>
2025-07-11 12:39:42 +0530

    Static payment page

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 // components/CCForm.js
 import { useState } from 'react';
+import "./assessement.css";
 
 
 
@@ -13,8 +14,8 @@ export default function CCForm() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        amount: '100.00',
-        billing_name : 'John Doe',        
+        amount: '25000.00',
+        billing_name: 'John Doe',
       }),
     });
     const result = await res.json();
@@ -23,10 +24,18 @@ export default function CCForm() {
   };
 
   return (
-    <div>
-      <button onClick={handlePayment} disabled={loading}>
-        {loading ? 'Redirecting...' : 'Pay Now'}
-      </button>
+    <div className="second_div_layout">
+      <div className="d-flex flex-column justify-content-between second_text_div">
+        <div className="d-flex flex-column justify-content-between innerdiv">
+          <div className="submit-btn-div paymentbtn">
+            <button className="btnstyle btn-submit" onClick={handlePayment} disabled={loading}>
+              {loading ? 'Redirecting...' : 'Pay Now'}
+            </button>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
