2c087b2387901b549767c78396ed7314a5835123
Nisha Patel <nisha.laniya@bypeopletechnologies.com>
2026-07-07 17:52:26 +0530

    Update page.js

--- src/app/basecamp/page.js
+++ src/app/basecamp/page.js
@@ -157,34 +157,38 @@ export default function BasecampPage() {
   const items = [
     {
       id: 1,
+      src: " https://youtu.be/nlLClO0F3JE",
+    },
+    {
+      id: 2,
       title: "Success story of #1 - Ador powerton",
       description:
         "Ravin Mirchandani, the Executive Chairperson talks about a remarkable success story about the impact of the Scaling Up performance system & the amazing business turnaround experienced within Ador Powertron. In the last section, Ravin talks about the things to keep in mind while selecting a good coach & the value added by the current coach.",
       src: " https://youtu.be/GyCb1FzL8f0",
     },
     {
-      id: 2,
+      id: 3,
       title: "Success Story #2 - Aliens Tattoo",
       description:
         "In this video, Sunny Bhanushali the Founder & CEO of Aliens Tattoo talks about experiencing challenges of rapid growth, the biggest benefit of implementing a structured framework & the value added by their coach.",
       src: "https://youtu.be/yZYbwIRNFGo",
     },
     {
-      id: 3,
+      id: 4,
       title: "Success Story #3 - Grauer & Weil (Engineering Division)",
       description:
         "Rohit More, the Director talks about the biggest benefits of implementing the Scaling Up framework. Besides ensuring alignment at every level within the organization, they clearly articulated their B-HAG (Big Hairy Audacious Goal) & have been systematically seeing growth YoY for the past 3 years. At the end, there is a reference to the Coach and the support provided in this growth journey.",
       src: "https://youtu.be/tVbpVpNADlw",
     },
     {
-      id: 4,
+      id: 5,
       title: "Success Story #4 - Clarion Technologies",
       description:
         "In this video, their CEO, Siddharth Motiwale talks about the 'secret sauce' which has helped Clarion grow systematically over the past few years. He also talks about the value added by Ajay as their coach.",
       src: "https://youtu.be/WO1A4s2Zjy8",
     },
     {
-      id: 5,
+      id: 6,
       title: "Success Story #5 - InfraCloud Technologies",
       description:
         "In this video, Girish Shilamkar, the Founder & CEO, shares some key aspects of the Scaling Up framework, which helped him build a great culture within his company. These foundations ultimately helped ensure a successful exit in 2025.",
@@ -192,7 +196,7 @@ export default function BasecampPage() {
         "https://youtu.be/kVqcCXjmE4w",
     },
     {
-      id: 6,
+      id: 7,
       title: "Success Story #6 - Uplers & Mavlers",
       description:
         "In this video, Jaymin (CEO) & Nital (COO) talk about the benefits of implementing the Scaling Up framework. They ensured alignment at all levels within their business & experienced the resultant growth.",
